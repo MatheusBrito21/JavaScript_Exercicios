e89b03e0b30da84c7d7f5ae03f5b96288f5a8b1a
Matheus Brito <matheusb790@gmail.com>
2022-07-26 09:57:14 -0300

    Modulos JS

---
+++ assets/javascript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import {mostraCidade,mostraIdade,mostraHobby} from './funcoes.js';
+
+console.log(mostraCidade('Matheus','Brasilia'));
+console.log(mostraHobby('Matheus', 'jogar videogame'));
+console.log(mostraIdade('Matheus', '24'));
