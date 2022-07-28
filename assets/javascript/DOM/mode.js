949978f5279740b6ff944571db35ad6ea073dfb9
Matheus Brito <matheusb790@gmail.com>
2022-07-28 08:33:08 -0300

    Manipulando a DOM

--- assets/javascript/DOM/mode.js
+++ assets/javascript/DOM/mode.js
@@ -1,8 +1,8 @@
 const darkModeClass = 'dark-mode';
 const button = document.getElementById('mode-selector');
 const h1 = document.getElementById('page-title');
-const corpo = document.getElementsByTagName('body')[0];
-const rodape = document.getElementsByTagName('footer')[0];
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
 
 
 function mudaModo(){
@@ -11,23 +11,23 @@ function mudaModo(){
 }
 
 function mudaClasse(){
-    h1.classList.toggle('darkModeClass');
-    button.classList.toggle('darkModeClass');
-    corpo.classList.toggle('darkModeClass');
-    rodape.classList.toggle('darkModeClass');
+    h1.classList.toggle(darkModeClass);
+    button.classList.toggle(darkModeClass);
+    body.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
 }
 
 function mudaTexto(){
     let light = 'Light Mode';
     let dark = 'Dark Mode';
 
-    if(corpo.classList.contains(darkModeClass)){
+    if(body.classList.contains(darkModeClass)){
         button.innerHTML = light;
-        h1.innerHTML = dark + 'ON';
+        h1.innerHTML = dark + ' ON';
         return;
     }
         button.innerHTML = dark ;
-        h1.innerHTML = light+ 'ON';
+        h1.innerHTML = light+ ' ON';
     
 }
 
